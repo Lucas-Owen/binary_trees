900e914e29bf19dee91bd8be8fd2ef734758a5a4
ambrose <kolus7381@gmail.com>
2023-08-30 00:41:57 +0100

    new_commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,7 +5,7 @@
  * @tree: pointer to the root node of the tree
  * Return: tree is NULL return 0 else height of tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+/*size_t binary_tree_height(const binary_tree_t *tree)
 {
 	binary_tree_t *left_t, *right_t;
 
@@ -29,4 +29,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (height_r > height_l)
 		return (height_r);
 	return (height_l);
+}*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    /* Recursively calculate the heights of the left and right subtrees */
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    /* Return the maximum of the left and right subtree heights, plus 1 for the root */
+    return (1 + (left_height > right_height ? left_height : right_height));
 }
