aed960d5a0f2946283636496fc3f2c2c891edb6f
= <=>
2023-08-29 17:19:03 +0300

    Create 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ *
+ * @parent: Pointer to the parent node of the node to create
+ * @value: Value to put in the new node
+ * Return: binary_tree_t* - pointer to the new node, or NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node)
+	{
+		node->n = value;
+		node->parent = parent;
+		node->left = NULL;
+		node->right = NULL;
+		if (parent)
+		{
+			if (value < parent->n)
+				parent->left = node;
+			else if (value > parent->n)
+				parent->right = node;
+			else
+				free(node), node = NULL;
+		}
+	}
+	return (node);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -11,10 +11,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -23,5 +23,7 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
