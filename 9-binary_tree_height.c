e40aa026eafffab8d19fcb0d714f1a0f29d5db80
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-08-30 11:19:53 +0300

    Rewrite binary_tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,26 +7,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	binary_tree_t *left_t, *right_t;
+	size_t height_l;
+	size_t height_r;
 
-	size_t height_r = 0;
-	size_t height_l = 0;
-
-	if (!tree)
-		return (0);
-	left_t = tree->left;
-	right_t = tree->right;
-	while (left_t != NULL)
-	{
-		height_l++;
-		left_t = left_t->left;
-	}
-	while (right_t != NULL)
-	{
-		height_r++;
-		right_t = right_t->right;
-	}
-	if (height_r > height_l)
-		return (height_r);
-	return (height_l);
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
