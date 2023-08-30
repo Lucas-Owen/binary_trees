ada4011e65c91c1cef74565c2fb9db28d6456968
= <=>
2023-08-30 18:25:59 +0300

    Add NULL check for tree

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ * new_list - Requests memory for a new list and exits on failure
+ * Return: my_list* - Pointer to the new my_list
+ */
+my_list *new_list()
+{
+	my_list *ptr = malloc(sizeof(my_list));
+
+	if (ptr)
+		return (ptr);
+	exit(1);
+}
+
+/**
+ * append_node - Appends a new node after the given node
+ * @last: Last node of a list
+ * @tree: Value of tree at new list node
+ */
+void append_node(my_list *last, binary_tree_t *tree)
+{
+	last->next = new_list();
+	last = last->next;
+	last->tree = tree;
+	last->next = NULL;
+}
+
+/**
  * binary_tree_levelorder - Performs a levelorder traversal on the tree
  *
  * @tree: Pointer to the root of the tree
@@ -8,28 +34,28 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	my_list *nodes = malloc(sizeof(my_list));
-	my_list *last = nodes, *curr = nodes;
+	my_list *nodes, *last, *curr;
 
+	if (!tree)
+		return;
+
+	nodes = new_list();
+	last = nodes, curr = nodes;
 	nodes->tree = tree;
 	nodes->next = NULL;
 	while (curr)
 	{
+		func(curr->tree->n);
 		if (curr->tree->left)
 		{
-			last->next = malloc(sizeof(my_list));
-			last->next->tree = curr->tree->left;
-			last->next->next = NULL;
+			append_node(last, curr->tree->left);
 			last = last->next;
 		}
 		if (curr->tree->right)
 		{
-			last->next = malloc(sizeof(my_list));
-			last->next->tree = curr->tree->right;
-			last->next->next = NULL;
+			append_node(last, curr->tree->right);
 			last = last->next;
 		}
-		func(curr->tree->n);
 		curr = curr->next;
 	}
 	while (nodes)
